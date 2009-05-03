a500f3907b74f75b5a003102d8a0cbedca6cbe52
dakrone <lee-at-writequit.org>
2009-05-03 08:46:28 -0600

    switched to using restclient instead of httparty for ease of use. Better error catching and added the authenticate method.

--- ricepaper.rb
+++ ricepaper.rb
@@ -4,18 +4,9 @@
 # Also works as a library in a pinch
 
 require 'rubygems'
-# For API ease
-require 'httparty'
-# Parsing command-line options
-require 'optparse'
+require 'restclient'
 
-# This class actually does the api posting
-class RicePaperAPI
-  include HTTParty
-  base_uri 'https://www.instapaper.com/api'
-end
-
-# Encapsulation class for libraries or command-line use
+# Encapsulation class
 class RicePaper
   attr_accessor :username, :password
   attr_reader :error
@@ -26,35 +17,56 @@ class RicePaper
     @error = ""
   end
 
+  def authenticate
+    RestClient.post "https://www.instapaper.com/api/authenticate", :username => @username, :password => @password
+  rescue RestClient::RequestFailed
+    e = $!.to_s
+    e =~ /(\d+)/
+      result = $1.to_s
+    @error = case result
+               when "400" : "Bad Request"
+               when "403" : "Invalid username or password"
+               when "500" : "The service encountered an error. Please try again later."
+               else "An unknown error occured"
+             end
+    return false
+  end
+
   def add(url=nil, title=nil)
     return if url.nil?
     result = ""
     @error = ""
-    begin
-      if title.nil?
-        result = RicePaperAPI.post('/add', :query => {:username => @username, :password => @password, :url => url, :"auto-title" => 1})
-      else
-        result = RicePaperAPI.post('/add', :query => {:username => @username, :password => @password, :url => url, :title => title})
-      end
-    rescue Net::HTTPServerException
-      # Convert an exception to (hopefully), a result we can put in 'error'
-      e = $!.to_s
-      e =~ /^(\d+) /
-        result = $1.to_s
+    if title.nil?
+      result = RestClient.post "https://www.instapaper.com/api/add", :username => @username,
+        :password => @password,
+        :url => url,
+        :"auto-title" => 1
+    else
+      result = RestClient.post "https://www.instapaper.com/api/add", :username => @username,
+        :password => @password,
+        :url => url,
+        :title => title
     end
-    return true if result == "201"
+  rescue RestClient::RequestFailed
+    # Convert an exception to (hopefully), a result we can put in 'error'
+    e = $!.to_s
+    e =~ /(\d+)/
+      result = $1.to_s
     @error = case result
              when "400" : "Bad Request"
              when "403" : "Invalid username or password"
              when "500" : "The service encountered an error. Please try again later."
              else "An unknown error occured"
-    end
+             end
     return false
   end
 end
 
 # Only run CLI if actually running the script instead of requiring it.
 if __FILE__ == $0
+  # Parsing command-line options
+  require 'optparse'
+
   opthash = Hash.new
   options = OptionParser.new do |opts|
     opts.banner = "Usage: #{$0} [options] \"<url>\" [\"<url>\" \"<url>\" ...]"
@@ -64,22 +76,37 @@ if __FILE__ == $0
     opts.on("-p", "--password [PASS]", "Password") do |opt|
       opthash['password'] = opt
     end
+    opts.on("-a", "--authenticate", "Only attempt to authenticate to Instapaper") do |opt|
+      opthash['authenticate'] = opt
+    end
     opts.on("-t", "--title [TITLE]", "Optional title for instantpaper entry") do |opt|
       opthash['title'] = opt
     end
   end
 
   options.parse!(ARGV)
+  rp = RicePaper.new(opthash['username'],opthash['password'])
+
+  if opthash['authenticate']
+      print "Authenticating..."
+      result = rp.authenticate
+      puts result ? "Successful." : "Failed."
+      if !result
+        puts "Error: #{rp.error}"
+      end
+      exit(0)
+  end
+
   if ARGV.size < 1
     puts "I need a url to add to InstantPaper"
   end
 
-  rp = RicePaper.new(opthash['username'],opthash['password'])
   ARGV.each do |url|
-    print "Submitting '#{url}'..."
     if opthash['title']
+      print "Submitting '#{url}' with title '#{opthash['title']}'..."
       result = rp.add(url,opthash['title'])
     else
+      print "Submitting '#{url}'..."
       result = rp.add(url)
     end
     puts result ? "Successful." : "Failed."
